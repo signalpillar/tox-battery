b30b0f60fa098e930aba8775a90f1d1e47cd8881
Volodymyr Vitvitskyi <contact.volodymyr@gmail.com>
2020-01-27 21:23:35 +0000

    [pip>=20] add support for a new version of pip
    Note: We shouldn't really depend on internals of the library as they don't
    provide any support for those. Suggested solution is to call `pip freeze`
    command to get the dependencies parsed.

--- toxbat/requirements.py
+++ toxbat/requirements.py
@@ -30,19 +30,23 @@ Name of the version file is composed from 2 parts:
     creation procedure.
 """
 
-# std
 import hashlib
 import os
 
-# 3rd-party
 try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
+    # pip >= 20
+    from pip._internal.network.session import PipSession
     from pip._internal.req.req_file import parse_requirements
+except ImportError:
+    try:
+        # pip < 10
+        from pip.download import PipSession
+        from pip.req import parse_requirements
+    except ImportError:
+        # It is quick hack to support pip 10 that has changed its internal
+        # structure of the modules.
+        from pip._internal.download import PipSession
+        from pip._internal.req.req_file import parse_requirements
 
 from tox import hookimpl
 
