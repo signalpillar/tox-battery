3b3ed97a875137f53031a506a3a5c53940bd7d9f
Volodymyr Vitvitskyi <contact.volodymyr@gmail.com>
2020-05-17 12:07:53 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Additional functionality for tox",
     long_description=content_of("README.rst"),
     license="http://opensource.org/licenses/MIT",
-    version="0.5.2",
+    version="0.6.0",
     author="Volodymyr Vitvitskyi",
     author_email="contact.volodymyr@gmail.com",
     url="https://github.com/signalpillar/tox-battery",
